0ab22c7d4b1a9287c4902d27bd47d31e1ecd9416
Matt Smith <mcs@darkregion.net>
2023-05-07 01:29:34 -0500

    Added import-tls-cert.go

---
+++ import-tls-cert.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"time"
+	"os"
+
+	"github.com/google/goexpect"
+)
+
+const (
+	timeout = 1 * time.Minute
+)
+
+var (
+	promptRE = regexp.MustCompile(">>>")
+	cert_pem_RE = regexp.MustCompile("Enter the path to the cert.pem file")
+	key_pem_RE  = regexp.MustCompile("Enter the path to the key.pem file")
+)
+
+func main() {
+	e, _, err := expect.Spawn("/usr/local/bin/proton-bridge-cli", -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer e.Close()
+
+	e.Expect(promptRE, timeout)
+	e.Send("import-tls-cert\n")
+	e.Expect(cert_pem_RE, timeout)
+	e.Send(os.Args[1] + "\n")
+	e.Expect(key_pem_RE, timeout)
+	e.Send(os.Args[2] + "\n")
+
+	e.Expect(promptRE, timeout)
+	e.Send("exit\n")
+}
+
